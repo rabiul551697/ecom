76b27cc6db0e9d10db239478fa59f1368f11ef7a
Md Rabiul Islam <84383706+rabiul551697@users.noreply.github.com>
2021-05-19 19:13:12 +0600

    summary

---
+++ resources/templates/front/header.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<title>tpsell.com</title>
+<!-- css custom link-->
+<link rel="stylesheet" href="css/styles.css">
+ <!--javascript custom link-->
+<script src="Js/script.js" defer></script>
+<!--favicon link-->
+<link rel="icon" href="images/tpsellc.png" type="image/gif" sizes="16x16">
+<!--Font Awesome-->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
+<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+
+</head>
+
+<!--  Start Body content-->
+<body>
+  <!-- messanger-->
+  <a href="https://www.facebook.com/faceittechnology" class="message"><i class="fa fa-comments"></i>
+  </a>
+<!--  top row-->
+        <div class="fix">
+        <div class="top-row">
+          <div class="search-box">
+              <input type="text" name="" value=""
+              placeholder="Search" class="search">
+              <button type="Submit" name="button" class="search-btn">
+                <i class="fa fa-search"></i>
+              </button>	&nbsp;&nbsp;&nbsp;
+              <i class="fa fa-phone">&nbsp;+8801575632763</i>&nbsp;&nbsp;
+              <i class="fa fa-envelope">&nbsp;tpsellc@gmail.com</i>
+          </div>
+        </div>
+
+<!--  Navbar Content-->
+        <nav class="navbar">
+        <div class="brand-title"><img src="images/tpsellc.png" alt=""></div>
+
+          <div class="dropdown-fix">
+          <div class="dropdown">
+          <button class="dropbtn">All Categories</button>
+          <div class="dropdown-content">
+          <a href="#">Pant</a>
+          <a href="#">T-shirt</a>
+          <a href="#">Desktop</a>
+          <a href="#">Mobile</a>
+          </div>
+        </div>
+        </div>
+
+        <a href="#" class="toggle-button">
+          <span class="bar"></span>
+          <span class="bar"></span>
+          <span class="bar"></span>
+        </a>
+
+        <div class="navbar-links">
+          <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Products</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Contact</a></li>
+            <li><a href="#">Login</a></li>
+            <li><a href="#"><img  src="images/cart.png" alt=""  class="menu-icon"></a></li>
+          </ul>
+        </div>
+      </nav>
+</div>
