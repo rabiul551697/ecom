f6d9e27f35bf6152bfd05e5b71587141127eae66
Md Rabiul Islam <84383706+rabiul551697@users.noreply.github.com>
2021-05-20 13:01:56 +0600

    summary

---
+++ resources/templates/front/SliderContent.php
@@ -0,0 +1,16 @@
+<div class="header">
+<div class="container">
+<div class="row">
+<div class="col-2">
+  <h1>Give your Workout
+  <br> A new style!</h1>
+  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut quis neque consequat, varius neque vitae,
+  <br>imperdiet libero. Nunc sed ligula congue, posuere quam bibendum,</p>
+  <a href="#" class="btn"> Shop Now &#8594;</a>
+</div>
+<div class="col-2">
+<img src="images/image1.png" alt="">
+</div>
+</div>
+</div>
+</div>
