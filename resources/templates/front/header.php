142e1a7188f89c55ad4523e5fb3ec0e2eecede47
Md Rabiul Islam <84383706+rabiul551697@users.noreply.github.com>
2021-05-20 16:33:46 +0600

    summary

--- resources/templates/front/header.php
+++ resources/templates/front/header.php
@@ -38,36 +38,9 @@
         </div>
 
 <!--  Navbar Content-->
-        <nav class="navbar">
-        <div class="brand-title"><img src="images/tpsellc.png" alt=""></div>
-
-          <div class="dropdown-fix">
-          <div class="dropdown">
-          <button class="dropbtn">All Categories</button>
-          <div class="dropdown-content">
-          <a href="#">Pant</a>
-          <a href="#">T-shirt</a>
-          <a href="#">Desktop</a>
-          <a href="#">Mobile</a>
-          </div>
-        </div>
-        </div>
-
-        <a href="#" class="toggle-button">
-          <span class="bar"></span>
-          <span class="bar"></span>
-          <span class="bar"></span>
-        </a>
-
-        <div class="navbar-links">
-          <ul>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">Products</a></li>
-            <li><a href="#">About</a></li>
-            <li><a href="#">Contact</a></li>
-            <li><a href="#">Login</a></li>
-            <li><a href="#"><img  src="images/cart.png" alt=""  class="menu-icon"></a></li>
-          </ul>
-        </div>
-      </nav>
+<?php
+//navbar
+include ("../resources/templates/front/navbar.php");
+?>
+      
 </div>
