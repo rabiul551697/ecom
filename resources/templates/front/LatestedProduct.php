449a140cc56a601a2796b31430a73bde05e64ac3
Md Rabiul Islam <84383706+rabiul551697@users.noreply.github.com>
2021-05-21 21:53:43 +0600

    update l product

--- resources/templates/front/LatestedProduct.php
+++ resources/templates/front/LatestedProduct.php
@@ -1,12 +1,28 @@
 <h2 class="title">Letest Products</h2>
 
-<div class="row">
+<?php
+//Latested product php
+$query = "SELECT * FROM products";
+$product_query = mysqli_query ($connection, $query);
+if (!$product_query) {
+       die ("QUERY FAILED" . mysqli_error ($connection));
+     }
+while ($row = mysqli_fetch_assoc($product_query)) {
+$product_id          = $row ['product_id'];
+$product_cat_id      = $row ['product_cat_id'];
+$product_title       = $row ['product_title'];
+$product_img         = $row ['product_img'];
+$product_description = $row ['product_description'];
+$product_pirce       = $row ['product_price'];
 
+ ?>
+
+<div class="row">
   <div class="col-4">
-    <img src="images/product-5.jpg" alt="">
-    <h4 style="color:#ff523b;">Red Prointed T-Shirt</h4>
-    <p>Lorem Ipsum is simply dummy</p>
-    <h3 > Price: &#2547; 50.00</h3>
+    <img src="images/<?php echo $product_img;?>" alt="">
+    <h4 style="color:#ff523b;"><?php echo $product_title; ?></h4>
+    <p><?php echo $product_descriptione; ?></p>
+    <h3 > Price: &#2547; <?php echo $product_price; ?></h3>
     <div class="rating">
     <i class="fa">&#9733;</i>
     <i class="fa">&#9733;</i>
@@ -17,3 +33,7 @@
     </div>
   </div>
 </div>
+
+<?php
+}
+ ?>
