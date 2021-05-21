a8571ff4baa14e4928d1dbcdc8fb220b870f928a
Md Rabiul Islam <84383706+rabiul551697@users.noreply.github.com>
2021-05-21 20:56:07 +0600

    update

--- resources/templates/front/LatestedProduct.php
+++ resources/templates/front/LatestedProduct.php
@@ -4,7 +4,7 @@
 
   <div class="col-4">
     <img src="images/product-5.jpg" alt="">
-    <h4>Red Prointed T-Shirt</h4>
+    <h4 style="color:#ff523b;">Red Prointed T-Shirt</h4>
     <p>Lorem Ipsum is simply dummy</p>
     <h3 > Price: &#2547; 50.00</h3>
     <div class="rating">
@@ -13,7 +13,7 @@
     <i class="fa">&#9733;</i>
     <i class="fa">&#9733;</i>
     <i class="fa">&#10032;</i>
-    <span style="padding-left:10px;"> 12 reviews</span>
+    <span style="padding-left:10px; color:#ff523b; font-weight:bold;"> 12 reviews</span>
     </div>
   </div>
 </div>
