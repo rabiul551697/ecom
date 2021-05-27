2e878b6baefd2f9ab474618f1c88a6f1f8163904
Md Rabiul Islam <84383706+rabiul551697@users.noreply.github.com>
2021-05-27 15:33:31 +0600

    login

---
+++ public/login.php
@@ -0,0 +1,49 @@
+<?php
+//Database connected
+include ("../resources/config.php");
+?>
+<?php
+//header Connection link
+include ("../resources/templates/front/header.php");
+?>
+<!-- content-->
+<div class="account-page">
+  <div class="container">
+    <div class="row">
+
+      <div class="col-2">
+        <img src="images/image1.png" alt="">
+      </div>
+      <div class="col-2">
+        <div class="form-container">
+            <div class="form-btn">
+              <span onclick="login()"> Seller Login</span>
+              <span onclick="register()"> Seller Register</span>
+              <hr id="Indicator">
+                </div>
+
+              <form action="" id="LoginForm">
+                <input type="text" name="" value="" placeholder="user name">
+                <input type="password" name="" value="" placeholder="password">
+                <button type="submit" name="button" class="btn">Login</button>
+                <a href="">Forgot Password</a>
+              </form>
+
+              <form action="" id="RegisterForm">
+                <input type="text" name="" value="" placeholder="user name">
+                <input type="email" name="" value="" placeholder="email">
+                <input type="number" name="" value="" placeholder="phone number">
+                <textarea name="" rows="5" cols="31" placeholder="enter your address"></textarea>
+                <button type="submit" name="button" class="btn">Register</button>
+              </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- footer-->
+<?php
+//header Connection link
+include ("../resources/templates/front/footer.php");
+?>
